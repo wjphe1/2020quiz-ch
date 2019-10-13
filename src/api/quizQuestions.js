c553b74af23540690349476b26bd8e5ff3d1c522
WeiJun Phee <lawliet5145@gmail.com>
2019-10-13 23:09:39 +0800

    7

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -30,6 +30,48 @@ var quizQuestions = [
         ]
     },
     {
+        question: "What is your yearly income range?",
+        answers: [
+            {
+                type: "lowincome",
+                content: "<RM20,000"
+            },
+            {
+                type: "midincome",
+                content: "RM20,000 - RM100,000"
+            },
+            {
+                type: "highincome",
+                content: "RM100,000 - RM2,000,000"
+            },
+            {
+                type: "higherincome",
+                content: ">RM2,000,000"
+            },
+            {
+                type: "stopwork",
+                content: "I'm not working."
+            }
+        ]
+    },
+    {
+        question: "Do you have an EPF account? Are you planning to withdraw your EPF for higher education?",
+        answers: [
+            {
+                type: "yesepfyes",
+                content: "Yes i have an EPF account and planning to withdraw for studies."
+            },
+            {
+                type: "yesepfno",
+                content: "Yes i have an EPF account but not planning to withdraw."
+            },
+            {
+                type: "noepf",
+                content: "No i do not have an EPF account."
+            }
+        ]
+    },
+    {
         question: "Are you planning to start your own business?",
         answers: [
             {
@@ -56,6 +98,36 @@ var quizQuestions = [
             {
                 type: "Public",
                 content: "Public transport"
+            }
+        ]
+    },
+    {
+        question: "Are you planning to sell or buy a house?",
+        answers: [
+            {
+                type: "buy",
+                content: "I'm planning to buy."
+            },
+            {
+                type: "sell",
+                content: "I'm planning to sell."
+            },
+            {
+                type: "notbuysell",
+                content: "I'm not buying nor selling."
+            }
+        ]
+    },
+    {
+        question: "Are you subscribed to Netflix, Spotify or been spending on gaming platform such as Steam?",
+        answers: [
+            {
+                type: "yesdigital",
+                content: "Yes"
+            },
+            {
+                type: "nodigital",
+                content: "No"
             }
         ]
     }
--- src/components/Result.js
+++ src/components/Result.js
@@ -3,15 +3,7 @@ import PropTypes from 'prop-types';
 import { CSSTransitionGroup } from 'react-transition-group';
 
 function Y1(props) {
-    return <CSSTransitionGroup
-    className="container result"
-    component="div"
-    transitionName="fade"
-    transitionEnterTimeout={800}
-    transitionLeaveTimeout={500}
-    transitionAppear
-    transitionAppearTimeout={500}
-  >
+    return <CSSTransitionGroup className="container result" component="div" transitionName="fade" transitionEnterTimeout={800} transitionLeaveTimeout={500} transitionAppear transitionAppearTimeout={500}>
     <div>
     ● RM735 million proposed for school maintenance and upgrading works.<br />
     ● Government allocates RM210m to expedite digital infrastructure establishment in public buildings like schools.
@@ -19,32 +11,34 @@ function Y1(props) {
   </CSSTransitionGroup>;
 }
 
-function A1(props) {
-    return <CSSTransitionGroup
-    className="container result"
-    component="div"
-    transitionName="fade"
-    transitionEnterTimeout={800}
-    transitionLeaveTimeout={500}
-    transitionAppear
-    transitionAppearTimeout={500}
-  >
+function Freshgrads(props) {
+    return <CSSTransitionGroup className="container result" component="div" transitionName="fade" transitionEnterTimeout={800} transitionLeaveTimeout={500} transitionAppear transitionAppearTimeout={500}>
     <div>
-    ● Hiring fresh graduates: Two-year pay incentives of RM500 a month. Hiring incentive of RM300 a month.
+    ● Hiring fresh graduates who have been unemployed for more than 12 months:
+    Two-year pay incentives of RM500 a month. Employers get two-year hiring incentives of RM300 a month.
     </div>
   </CSSTransitionGroup>;
 }
 
-function A2(props) {
-    return <CSSTransitionGroup
-    className="container result"
-    component="div"
-    transitionName="fade"
-    transitionEnterTimeout={800}
-    transitionLeaveTimeout={500}
-    transitionAppear
-    transitionAppearTimeout={500}
-  >
+function BSH(props) {
+  return <CSSTransitionGroup className="container result" component="div" transitionName="fade" transitionEnterTimeout={800} transitionLeaveTimeout={500} transitionAppear transitionAppearTimeout={500}>
+  <div>
+  ● BSH scheme expanded to cover 1.1 million single individuals aged above 40 earning less than RM2,000 per month.
+  </div>
+</CSSTransitionGroup>;
+}
+
+function MySalam(props) {
+  return <CSSTransitionGroup className="container result" component="div" transitionName="fade" transitionEnterTimeout={800} transitionLeaveTimeout={500} transitionAppear transitionAppearTimeout={500}>
+  <div>
+  ● MySalam to be expanded so that those with critical illnesses will get RM8,000 cash. <br/>
+  ● Those being treated in govt hospitals can also claim RM50 wage replacement a day for up to 14 days.
+  </div>
+</CSSTransitionGroup>;
+}
+
+function Ewallet(props) {
+    return <CSSTransitionGroup className="container result" component="div" transitionName="fade" transitionEnterTimeout={800} transitionLeaveTimeout={500} transitionAppear transitionAppearTimeout={500}>
     <div>
     ● To boost use of e-wallets, govt to offer one-time <strong>RM30</strong> digital stimulus to qualified Malaysians 
     aged <strong>18 and above</strong> with annual income less than RM100,000.
@@ -52,35 +46,39 @@ function A2(props) {
   </CSSTransitionGroup>;
 }
 
-function S1(props) {
-    return <CSSTransitionGroup
-    className="container result"
-    component="div"
-    transitionName="fade"
-    transitionEnterTimeout={800}
-    transitionLeaveTimeout={500}
-    transitionAppear
-    transitionAppearTimeout={500}
-  >
+function Sellhouse(props) {
+    return <CSSTransitionGroup className="container result" component="div" transitionName="fade" transitionEnterTimeout={800} transitionLeaveTimeout={500} transitionAppear transitionAppearTimeout={500}>
     <div>
-    ● Allocation for subsidies and social assistance increased to RM24.2 billion, 
-     including welfare aid such as Bantuan Sara Hidup (BSH). <br />
-    ● BSH scheme expanded to cover 1.1 million single individuals aged above 40 earning less than RM2,000 per month.
+    ● <strong>Real property gain tax (RPGT)</strong> base year for asset purchase revised from 1/1/2000 to 1/1/2013 for asset acquired before that date. 
+    I.E.: RPGT = (selling price of house – house evaluation price on 1/1/2013) x 5%. <br/>
+    ● To reduce supply overhang of condominiums and apartments amounting to RM8.3 billion in the second quarter of 2019, 
+    govt will lower the threshold on high rise property prices in urban areas for foreign ownership <strong>from RM1,000,000 to RM600,000</strong> in 2020.
+
     </div>
   </CSSTransitionGroup>;
 }
 
+function Youngbuyhouse(props) {
+  return <CSSTransitionGroup className="container result" component="div" transitionName="fade" transitionEnterTimeout={800} transitionLeaveTimeout={500} transitionAppear transitionAppearTimeout={500}>
+  <div>
+  ● Govt to extend <strong>Youth Housing Scheme</strong> administered by Bank Simpanan Nasional from 1/1/2020 until 31/12/2021. 
+  The scheme also offers a 10% loan guarantee via Cagamas to enable borrowers to get full financing and RM200 monthly 
+  instalment assistance for the first two years, limited to 10,000 home units.
+  </div>
+</CSSTransitionGroup>;
+}
+
+function Buyhouse(props) {
+  return <CSSTransitionGroup className="container result" component="div" transitionName="fade" transitionEnterTimeout={800} transitionLeaveTimeout={500} transitionAppear transitionAppearTimeout={500}>
+  <div>
+  ● To help those who can't come up with 10% deposit or get financing to buy homes, govt will collaborate with financial institutions to introduce the <strong>rent-to-own (RTO)</strong> financing scheme, 
+  where up to RM10 billion will be provided by the financial institutions, with the government supporting via a 30% or RM3 billion guarantee. (Terms applied)
+  </div>
+</CSSTransitionGroup>;
+}
 
 function Car(props) {
-    return <CSSTransitionGroup
-    className="container result"
-    component="div"
-    transitionName="fade"
-    transitionEnterTimeout={800}
-    transitionLeaveTimeout={500}
-    transitionAppear
-    transitionAppearTimeout={500}
-  >
+    return <CSSTransitionGroup className="container result" component="div" transitionName="fade" transitionEnterTimeout={800} transitionLeaveTimeout={500} transitionAppear transitionAppearTimeout={500}>
     <div>
     ● Citizens to enjoy average 18% discount on all PLUS highways.<br />
     ● Effective Jan 1, 2020, toll rates for cars at the Second Penang Bridge will be reduced from RM8.50 to RM7.00.
@@ -89,15 +87,7 @@ function Car(props) {
 }
 
 function Public(props) {
-    return <CSSTransitionGroup
-    className="container result"
-    component="div"
-    transitionName="fade"
-    transitionEnterTimeout={800}
-    transitionLeaveTimeout={500}
-    transitionAppear
-    transitionAppearTimeout={500}
-  >
+    return <CSSTransitionGroup className="container result" component="div" transitionName="fade" transitionEnterTimeout={800} transitionLeaveTimeout={500} transitionAppear transitionAppearTimeout={500}>
     <div>
     ● RM450 million proposed to acquire up to 500 electric buses for public transport in selected cities nationwide.<br />
     ● Govt intends to proceed with the Rapid Transit System (RTS) between Johor Bahru and Singapore.
@@ -106,15 +96,7 @@ function Public(props) {
 }
 
 function Petrol(props) {
-    return <CSSTransitionGroup
-    className="container result"
-    component="div"
-    transitionName="fade"
-    transitionEnterTimeout={800}
-    transitionLeaveTimeout={500}
-    transitionAppear
-    transitionAppearTimeout={500}
-  >
+    return <CSSTransitionGroup className="container result" component="div" transitionName="fade" transitionEnterTimeout={800} transitionLeaveTimeout={500} transitionAppear transitionAppearTimeout={500}>
     <div>
     ● Individuals owning not more than <strong>two cars</strong> and <strong>two motorcycles</strong> can get fuel subsidy for one vehicle. The qualifying criteria are:<br />
         ❖ A passenger car with 1,600cc engine capacity and below, or<br />
@@ -126,15 +108,7 @@ function Petrol(props) {
 }
 
 function WorkingFemale(props) {
-    return <CSSTransitionGroup
-    className="container result"
-    component="div"
-    transitionName="fade"
-    transitionEnterTimeout={800}
-    transitionLeaveTimeout={500}
-    transitionAppear
-    transitionAppearTimeout={500}
-  >
+    return <CSSTransitionGroup className="container result" component="div" transitionName="fade" transitionEnterTimeout={800} transitionLeaveTimeout={500} transitionAppear transitionAppearTimeout={500}>
     <div>
     ● Incentives to get women into the workforce:<br />
         ❖ Two-year pay incentive of RM500 a month<br />
@@ -145,15 +119,7 @@ function WorkingFemale(props) {
 }
   
 function Business(props) {
-    return <CSSTransitionGroup
-    className="container result"
-    component="div"
-    transitionName="fade"
-    transitionEnterTimeout={800}
-    transitionLeaveTimeout={500}
-    transitionAppear
-    transitionAppearTimeout={500}
-  >
+    return <CSSTransitionGroup className="container result" component="div" transitionName="fade" transitionEnterTimeout={800} transitionLeaveTimeout={500} transitionAppear transitionAppearTimeout={500}>
     <div>
     ● RM445m <strong>Bumiputera</strong> entrepreneur development grant for access to financing, provision of business premises, entrepreneurship training.<br />
     ● Govt to provide loans worth RM100m under Small Industries Entrepreneurs Financing Scheme for <strong>Chinese</strong> community.<br />
@@ -162,33 +128,68 @@ function Business(props) {
   </CSSTransitionGroup>;
 }
 
+function EPF(props) {
+  return <CSSTransitionGroup className="container result" component="div" transitionName="fade" transitionEnterTimeout={800} transitionLeaveTimeout={500} transitionAppear transitionAppearTimeout={500}>
+  <div>
+  ● To encourage adult learning, the EPF will expand the scope of its education withdrawal for qualification attained at certificate level, 
+  especially for accredited programmes that are in line with the nation’s IR 4.0 aspirations.
+  </div>
+</CSSTransitionGroup>;
+}
+
+function DST(props) {
+  return <CSSTransitionGroup className="container result" component="div" transitionName="fade" transitionEnterTimeout={800} transitionLeaveTimeout={500} transitionAppear transitionAppearTimeout={500}>
+  <div>
+  ● Digital services tax will be implemented with effect from 1/1/2020, 
+  to include services such as, but not limited to downloaded software, music, video, or digital advertising.
+  </div>
+</CSSTransitionGroup>;
+}
+
+function HIT(props) {
+  return <CSSTransitionGroup className="container result" component="div" transitionName="fade" transitionEnterTimeout={800} transitionLeaveTimeout={500} transitionAppear transitionAppearTimeout={500}>
+  <div>
+  ● Govt proposes that a new band for taxable income in excess of RM2,000,000 be introduced and taxed at <strong>30%</strong>, a 2% increment from the current 28%.
+  </div>
+</CSSTransitionGroup>;
+}
+
 function Result(props) {
     const finalresult = props.quizResult;
     var List = [];
+    List.push(<MySalam/>);
 
     if (finalresult.includes("Young") === true) {
-        List.push(<Y1 />)
-    } else if (finalresult.includes("Adult" && "Ownbusiness") === true) {
-        List.push(<A2/>)
-    } else if (finalresult.includes("Adult" && "Noownbusiness") === true) {
-        List.push(<A1/>,<A2/>)
-    }else {
-        List.push(<S1 />,<A2/>)
-    }
-
-    if (finalresult.includes("Female" && "Adult" && "Noownbusiness") === true) {
-        List.push(<WorkingFemale />);
-    }else if (finalresult.includes("Adult" && "Ownbusiness") === true) {
-        List.push(<Business />);
-    }
-    
+        List.push(<Y1/>)} 
+    if (finalresult.includes("Adult" && "stopwork") === true) {
+        List.push(<Freshgrads/>)} 
+    if (finalresult.includes("Senior" && "midincome") === true) {
+        List.push(<BSH/>)}
+    if (finalresult.includes("Young") === false) {
+        List.push(<Ewallet/>)}
+    if (finalresult.includes("sell") === true) {
+        List.push(<Sellhouse/>)}
+    if (finalresult.includes("buy") === true) {
+        List.push(<Buyhouse/>)}
+    if (finalresult.includes("buy" && "Adult") === true) {
+        List.push(<Youngbuyhouse/>)}
+    if (finalresult.includes("Female" && "Adult" && "stopwork") === true) {
+        List.push(<WorkingFemale />);}
+    if (finalresult.includes("Ownbusiness") === true) {
+        List.push(<Business />)}
+    if (finalresult.includes("yesepfyes") === true) {
+        List.push(<EPF/>)}   
     if (finalresult.includes("Car") === true) {
-        List.push(<Car/>);
-    }else if (finalresult.includes("Bike") === true) {
-        List.push(<Petrol />);
-    }else{
-        List.push(<Public />)
-    }
+        List.push(<Car/>,<Petrol/>)} 
+    if (finalresult.includes("Bike") === true) {
+        List.push(<Petrol/>)}
+    if (finalresult.includes("Public") === true) {
+        List.push(<Public/>)}
+    if (finalresult.includes("yesdigital") === true) {
+        List.push(<DST/>)}
+    if (finalresult.includes("higherincome") === true) {
+        List.push(<HIT/>)}
+
     return List;
 }
 
